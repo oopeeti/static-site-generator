06d91aa613640e2ed870c9c96672c8e2dcd95ba3
oopeeti <ollipekkanikka@gmail.com>
2025-01-11 17:23:28 +0200

    added static content

--- src/main.py
+++ src/main.py
@@ -2,10 +2,6 @@ from nodes import *
 from inline_markdown import split_nodes_delimiter
 
 def main():
-    node1 = TextNode("Normal text ", TextType.TEXT)
-    node2 = TextNode("bold text", TextType.BOLD)
-    old_nodes = [node1, node2]
-    new_nodes = split_nodes_delimiter(old_nodes, "*", TextType.ITALIC)  
-    
+    return 0
     
 main()
