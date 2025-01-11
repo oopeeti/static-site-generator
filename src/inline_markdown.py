d78ca58bd4745d814f1a0a05e0e15213c1c0691b
oopeeti <ollipekkanikka@gmail.com>
2025-01-11 17:20:23 +0200

    finished markdown to html functionality

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -18,8 +18,10 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
             continue
         split_nodes = []
         sections = old_node.text.split(delimiter)
+        
         if len(sections) % 2 == 0:
             raise ValueError("Invalid markdown, formatted section not closed")
+        
         for i in range(len(sections)):
             if sections[i] == "":
                 continue
@@ -27,6 +29,7 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                 split_nodes.append(TextNode(sections[i], TextType.TEXT))
             else:
                 split_nodes.append(TextNode(sections[i], text_type))
+                
         new_nodes.extend(split_nodes)
     return new_nodes
 
@@ -106,8 +109,6 @@ def split_nodes_link(old_nodes):
             
     return new_nodes
 
-
-    
 def extract_markdown_images(text):
     return re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
 
--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,3 +1,6 @@
+from nodes import HTMLNode
+from inline_markdown import *
+
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
     filtered_blocks = []
@@ -42,3 +45,122 @@ def block_to_block_type(block):
             i += 1
         return "ordered_list"
     return "paragraph"
+
+
+markdown = """# My Title
+
+This is a paragraph with *italic* and **bold** text.
+It also has a [link to Boot.dev](https://boot.dev) and an
+![image of a bear](https://example.com/bear.jpg) in it.
+
+* List item with **bold** text
+* List item with *italic* text
+* List item with a [link](https://boot.dev)
+
+1. Ordered list element one
+2. Ordered list element two
+3. Ordered list element three
+
+```This is code block```
+
+[link](https://boot.dev)
+
+> This is a quote with **bold** and *italic* text
+"""
+
+def text_to_children(text):
+    text_nodes = text_to_textnodes(text)
+    html_nodes = []
+    for node in text_nodes:
+            html_node = text_node_to_html_node(node)
+            html_nodes.append(html_node)
+    return html_nodes
+
+def markdown_to_html_node(markdown):
+    blocks = markdown_to_blocks(markdown)
+    children = []
+    
+    for block in blocks:
+        html_node = block_to_html_node(block)
+        children.append(html_node)
+        
+    return ParentNode("div", children, None)
+
+def paragraph_to_html_node(block):
+    lines = block.split("\n")
+    paragraph = " ".join(lines)
+    children = text_to_children(paragraph)
+    return ParentNode("p", children)
+
+def code_to_html_node(block):
+    if not block.startswith("```") or not block.endswith("```"):
+        raise ValueError("Invalid code block")
+    text = block.strip("```")
+    children = text_to_children(text)
+    code = ParentNode("code", children)
+    return ParentNode("pre", [code])
+
+def olist_to_html_node(block):
+    list_items = []
+    items = block.split("\n")
+    for item in items:
+        # Remove the * and leading space
+        item_text = item[item.find(".")+1:].strip()
+        item_children = text_to_children(item_text)
+        list_items.append(ParentNode("li", item_children))
+    return ParentNode("ol", list_items)
+
+def ulist_to_html_node(block):
+    items = block.split("\n")
+    list_items = []
+    for item in items:
+        # Remove the * and leading space
+        item_text = item.lstrip("*").lstrip("-").strip()
+        item_children = text_to_children(item_text)
+        list_items.append(ParentNode("li", item_children))
+    return ParentNode("ul", list_items)
+
+def quote_to_html_node(block):
+    lines = block.split("\n")
+    new_lines = []
+
+    for line in lines:
+        if not line.startswith(">"):
+            raise ValueError("Invalid quote block")
+        new_lines.append(line.lstrip(">").strip())
+        
+    content = " ".join(new_lines)
+    children = text_to_children(content)
+    return ParentNode("blockquote", children)
+
+def heading_to_html_node(block):
+    heading_level = block.count("#")
+    
+    if(heading_level >= len(block)):
+        raise ValueError(f"Invalid heading level: {heading_level}")
+    
+    # Remove the #s and leading space
+    text = block.lstrip("#").strip()
+    children = text_to_children(text)
+    return ParentNode(f"h{heading_level}", children)
+        
+def block_to_html_node(block):
+    block_type = block_to_block_type(block)
+
+    if block_type == "paragraph":
+        return paragraph_to_html_node(block)
+    elif block_type == "heading":
+        return heading_to_html_node(block)
+    elif block_type == "code":
+        return code_to_html_node(block)
+    elif block_type == "ordered_list":
+        return olist_to_html_node(block)
+    elif block_type == "unordered_list":
+        return ulist_to_html_node(block)
+    elif block_type == "quote":
+        return quote_to_html_node(block)
+            
+    raise ValueError("Invalid block type")
+     
+nodes = markdown_to_html_node(markdown)
+print(nodes)
