6b632a8a0354f7d9ee34e1bd6fa6a8f179be9dc5
oopeeti <ollipekkanikka@gmail.com>
2025-01-11 19:01:10 +0200

    added static to public copy functionality

--- src/main.py
+++ src/main.py
@@ -1,7 +1,28 @@
-from nodes import *
-from inline_markdown import split_nodes_delimiter
+import os
+import shutil
 
+PUBLIC_DIR_PATH = "./public"
+STATIC_DIR_PATH = "./static"
+
+def copy_directory(source, destination):
+    items = os.listdir(source)
+    for item in items:
+        item_path = os.path.join(source, item)
+        dest_item = os.path.join(destination, item)
+        print(f"[COPY] {item_path} ---> {dest_item}")
+        
+        if os.path.isfile(item_path):
+            shutil.copy(item_path, os.path.join(destination, item))
+        if os.path.isdir(item_path):
+            os.mkdir(os.path.join(destination, item))
+            
+            # Do the function recursively until no more files found
+            copy_directory(item_path, dest_item) 
+        
 def main():
-    return 0
+    if os.path.exists(PUBLIC_DIR_PATH):
+        shutil.rmtree(PUBLIC_DIR_PATH)
+    os.mkdir(PUBLIC_DIR_PATH)
     
+    copy_directory(STATIC_DIR_PATH, PUBLIC_DIR_PATH)
 main()
